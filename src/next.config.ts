923fbe43cfba12a6481ab583695afdb98f9aab61
Marwan Zidan <marwanzidan086@gmail.com>
2025-09-13 10:00:51 +0000

    واضح ان في حطا

---
+++ src/next.config.ts
@@ -0,0 +1,41 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  staticPageGenerationTimeout: 1000,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
+
+export default nextConfig;
