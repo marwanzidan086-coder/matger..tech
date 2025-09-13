83a82dd7385c6898d39d90c2382f6211cb379357
Marwan Zidan <marwanzidan086@gmail.com>
2025-09-13 20:01:44 +0000

    دي لينكات المنتج الثالث https://i.ibb.co/gb51bYgp/image-1.jpg https://i.

--- src/lib/data/product-images.ts
+++ src/lib/data/product-images.ts
@@ -19,17 +19,28 @@ export const productImages: ProductImageData[] = [
       "https://i.ibb.co/SDYLDd9/9.jpg",
       "https://i.ibb.co/fYgRnvN/R-1.jpg",
       "https://i.ibb.co/jZ34JYt/Q-1.jpg",
-      "https://i.ibb.co/d0NjmfB/f-1.jpg"
+      "https://i.ibb.co/d0NjmfB/f-1.jpg",
     ],
   },
   {
     id: 2,
-    images: ["https://picsum.photos/seed/2/600/600", `https://picsum.photos/seed/221/600/600`, `https://picsum.photos/seed/222/600/600`],
+    images: ["https://i.ibb.co/BVCkd46d/image-3.png", `https://i.ibb.co/3YCMfzT0/image-1.png`, `https://i.ibb.co/4gPz6vcw/image-2.jpg`, `https://i.ibb.co/9kSnv8yT/image-4.jpg`],
   },
   {
     id: 3,
-    images: ["https://picsum.photos/seed/3/600/600", `https://picsum.photos/seed/331/600/600`, `https://picsum.photos
-/seed/332/600/600`],
+    images: [
+      "https://i.ibb.co/gb51bYgp/image-1.jpg",
+      "https://i.ibb.co/DDfMBp6c/image-2.jpg",
+      "https://i.ibb.co/QvN3SGNs/image-3.jpg",
+      "https://i.ibb.co/d4sTzG4D/image-4.jpg",
+      "https://i.ibb.co/zT4kS2Pw/image-5.jpg",
+      "https://i.ibb.co/kgkPC2xy/image-6.jpg",
+      "https://i.ibb.co/Z1YVNpJb/image-7.jpg",
+      "https://i.ibb.co/prGjRdyW/image-8.jpg",
+      "https://i.ibb.co/q3v7wbpx/image-9.jpg",
+      "https://i.ibb.co/jPYkMQwR/image-10.jpg",
+      "https://i.ibb.co/cKVHxW3h/image-11.jpg"
+    ],
   },
   {
     id: 4,
@@ -37,7 +48,7 @@ export const productImages: ProductImageData[] = [
   },
   {
     id: 5,
-    images: ["https://i.ibb.co/67Xmd1v/image-5.jpg", `https://picsum.photos/seed/51/600/600`, `https://picsum.photos/seed/52/600/600`],
+    images: ["https://i.ibb.co/67Xmd1v0/image-5.jpg", `https://picsum.photos/seed/51/600/600`, `https://picsum.photos/seed/52/600/600`],
   },
   {
     id: 6,
