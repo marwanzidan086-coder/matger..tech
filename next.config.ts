361f0f3def26e238803b00aad2b0aea321f8f369
Marwan Zidan <marwanzidan086@gmail.com>
2025-09-13 09:48:04 +0000

    ده اللي جالي

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   staticPageGenerationTimeout: 1000,
   typescript: {
     ignoreBuildErrors: true,
